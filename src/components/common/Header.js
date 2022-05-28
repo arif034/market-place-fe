d309d51dcf25a06fb27750e1a513f0ac8a997772
Arif034 <arif.ece.jmi@gmail.com>
2022-05-28 12:11:32 +0530

    changes

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,20 +1,11 @@
-import {useState} from 'react'
-
 function Header() {
-
-    const [message, setMessage] = useState('')
-
-    function onChange(event){
-        console.log(event.target.value);
-        setMessage(event.target.value);
-    }
-
-    render()
+    function onChange(val)
     {
-        return<div>
-            <input type = "text" value = {message} onChange = {onChange}/>
-        </div>
+        console.log(val.target.value);
     }
+        return(<div>
+            <input type = "text" onChange = {onChange}/>
+        </div>);
 }
 
 export default Header;
