90f9d08b9e37e7364ddc4503d475ccfe2923846b
Arif034 <arif.ece.jmi@gmail.com>
2022-05-29 13:45:08 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Search from './components/home/Search.js';
 import AddProducts from './components/products/AddProducts';
+import './App.css';
 function App() {
   return (
-    <div>
-    <p>Welcome To Marketplace</p>
+    <div className='app'>
+    <p className = 'heading-main'>Welcome To Marketplace</p>
     <Search/>
     <AddProducts/>
     </div>
